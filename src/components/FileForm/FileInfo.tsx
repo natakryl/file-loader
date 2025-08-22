e81e475cdb05f548843eaeeb75d21a330ec0feba
natakryl <nanasha0007@gmail.com>
2025-08-22 16:20:54 +0300

    feat: add basic project structure
    - created folders: pages, components, services, utils, hooks
    - added placeholder files for future implementation:
      - HomePage, AuthPage
      - FileForm, FileInfo, ErrorMessage, SuccessLink
      - AuthService, YandexDiskApi, FileService
      - formatSize util
      - useFileUpload hook

---
+++ src/components/FileForm/FileInfo.tsx
@@ -0,0 +1,3 @@
+export default function FileInfo() {
+  return <div>FileInfo component</div>;
+}
---
+++ src/components/FileForm/SuccessLink.tsx
@@ -0,0 +1,3 @@
+export default function SuccessLink() {
+  return <div>SuccessLink component</div>;
+}
